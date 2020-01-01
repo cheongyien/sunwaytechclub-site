5c200e22b43fe8e384d81423060cf28e089aa35a
VenatusSimpleX <trollingwarfare@yahoo.com>
2020-01-01 15:12:40 +0800

    Laravel & Reactjs initial commit

--- src/redux/actions/index.js
+++
@@ -1,9 +0,0 @@
-export const selectPage = (store, selectedPage) => {
-  const page = selectedPage;
-  store.setState({ page });
-};
-
-export const toggleDrawer = (store, payload) => {
-  const drawerVisible = payload;
-  store.setState({ drawerVisible })
-}
